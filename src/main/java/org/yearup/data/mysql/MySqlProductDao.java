1249ebd2190c221e11b8eb7ba09aeb0b07180e84
jacobjdramos2 <jandek60@gmail.com>
2025-06-27 02:30:04 -0700

    Fixed and tested search/filter features: category, price, color.

--- src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -23,10 +23,18 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
     {
         List<Product> products = new ArrayList<>();
 
+        // String sql = "SELECT * FROM products " +
+        //         "WHERE (category_id = ? OR ? = -1) " +
+        //         "   AND (price <= ? OR ? = -1) " +
+        //         "   AND (color = ? OR ? = '') ";
+
+        // Updates upper and lower bounds
         String sql = "SELECT * FROM products " +
-                "WHERE (category_id = ? OR ? = -1) " +
-                "   AND (price <= ? OR ? = -1) " +
-                "   AND (color = ? OR ? = '') ";
+                "WHERE (? = -1 OR category_id = ?) " +
+                "AND (? = -1 OR price >= ?) " +
+                "AND (? = -1 OR price <= ?) " +
+                "AND (? = '' OR color = ?) ";
+            
 
         categoryId = categoryId == null ? -1 : categoryId;
         minPrice = minPrice == null ? new BigDecimal("-1") : minPrice;
@@ -35,13 +43,24 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
 
         try (Connection connection = getConnection())
         {
+            // PreparedStatement statement = connection.prepareStatement(sql);
+            // statement.setInt(1, categoryId);
+            // statement.setInt(2, categoryId);
+            // statement.setBigDecimal(3, minPrice);
+            // statement.setBigDecimal(4, minPrice);
+            // statement.setString(5, color);
+            // statement.setString(6, color);
+
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(1, categoryId);
             statement.setInt(2, categoryId);
             statement.setBigDecimal(3, minPrice);
             statement.setBigDecimal(4, minPrice);
-            statement.setString(5, color);
-            statement.setString(6, color);
+            statement.setBigDecimal(5, maxPrice);
+            statement.setBigDecimal(6, maxPrice);
+            statement.setString(7, color);
+            statement.setString(8, color);
+
 
             ResultSet row = statement.executeQuery();
 
